4a64e86760c86f3c4a7819eeda71c866083c799d
Matej634 <93345306+Matej634@users.noreply.github.com>
2021-11-08 18:03:16 +0100

    Add files via upload

---
+++ dz1_oop/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace dz1_oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunDemoForHW1();
+        }
+        private static void RunDemoForHW1()
+        {
+            Weather current = new Weather();
+            current.SetTemperature(24.12);
+            current.SetWindSpeed(3.5);
+            current.SetHumidity(0.56);
+            Console.WriteLine("Weather info:\n"
+                + "\ttemperature: " + current.GetTemperature() + "\n"
+                + "\thumidity: " + current.GetHumidity() + "\n"
+                + "\twind speed: " + current.GetWindSpeed() + "\n");
+            Console.WriteLine("Feels like: " + current.CalculateFeelsLikeTemperature());
+
+            Console.WriteLine("Finding weather info with largest windchill!");
+            const int Count = 5;
+            double[] temperatures = new double[Count] { 8.33, -1.45, 5.00, 12.37, 7.67 };
+            double[] humidities = new double[Count] { 0.3790, 0.4555, 0.743, 0.3750, 0.6612 };
+            double[] windSpeeds = new double[Count] { 4.81, 1.5, 5.7, 4.9, 1.2 };
+
+            Weather[] weathers = new Weather[Count];
+            
+            for (int i = 0; i < weathers.Length; i++)
+            {
+                weathers[i] = new Weather(temperatures[i], humidities[i], windSpeeds[i]);
+                Console.WriteLine("Windchill for weathers[" + i + "] is: " + weathers[i].CalculateWindChill());
+            }
+            
+            Weather largestWindchill = FindWeatherWithLargestWindchill(weathers);
+            Console.WriteLine(
+                "Weather info:" + largestWindchill.GetTemperature() + ", " +
+                largestWindchill.GetHumidity() + ", " + largestWindchill.GetWindSpeed()
+            );
+        }
+        private static Weather FindWeatherWithLargestWindchill(Weather [] weathers)
+        {
+            int i, x = 0;
+            double najveci = weathers[0].CalculateWindChill();
+            for (i = 0; i < weathers.Length; i++)
+            {
+                if(weathers[i].CalculateWindChill()>najveci)
+                {
+                    najveci = weathers[i].CalculateWindChill();
+                    x = i;
+                }
+            }
+            return weathers[x];
+        }
+    }
+}
---
+++ dz1_oop/Weather.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dz1_oop
+{
+    class Weather
+    {
+        double temperature;
+        double humidity;
+        double windSpeed;
+
+        public Weather(double temperature, double humidity, double windSpeed)
+        {
+            this.temperature = temperature;
+            this.humidity = humidity;
+            this.windSpeed = windSpeed;
+        }
+
+        public Weather() { }
+
+        public void SetTemperature(double temperature)
+        {
+            this.temperature = temperature;
+        }
+        public void SetWindSpeed(double windSpeed)
+        {
+            this.windSpeed = windSpeed;
+        }
+        public void SetHumidity(double humidity)
+        {
+            this.humidity = humidity;
+        }
+        public double GetTemperature()
+        {
+            return this.temperature;
+        }
+        public double GetHumidity()
+        {
+            return this.humidity;
+        }
+        public double GetWindSpeed()
+        {
+            return this.windSpeed;
+        }
+        public double CalculateFeelsLikeTemperature()
+        {
+            return -8.78469475556 + 1.61139411 * temperature + 2.33854883889 * humidity - 0.14611605 * temperature * humidity - 0.012308094 * (temperature * temperature) - 0.0164248277778 * (humidity * humidity) + 0.002211732 * (temperature * temperature) * humidity + 0.00072546 * temperature * (humidity * humidity) - 0.000003582 * (temperature * temperature) * (humidity * humidity);
+        }
+        public double CalculateWindChill()
+        {
+            if (temperature <= 10 && windSpeed > 4.8)
+            {
+                return 13.12 + 0.6215 * temperature - 11.37 * Math.Pow(windSpeed, 0.16) + 0.3965 * temperature * Math.Pow(windSpeed, 0.16);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        
+    }
+}
